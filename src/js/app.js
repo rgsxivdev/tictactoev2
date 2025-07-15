989cd5392913d7dbb4fad9385e5783458f7b62f9
Ruben Gonzales <rgsxivdev@gmail.com>
2025-07-14 18:37:10 -0700

    redid the generateGameboard function, added an init func

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,20 @@
-
-function generateGameboard(gridSize){
-    const grid = document.querySelector('.grid'); 
-    grid.innerHTML = "";
-    grid.style.setProperty('--grid-size', gridSize);
-    for (let i = 0; i < gridSize * gridSize; i++){
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.dataset.cell = i;
-    grid.appendChild(cell);
-   }
+function generateGameBoard(boardSize, board) {
+    board.innerHTML = '';
+    board.style.setProperty('--grid-size', boardSize);
+    for (let i = 0; i < boardSize * boardSize; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.dataset.cell = i;
+        board.appendChild(cell);
+    }
 }
 
-generateGameboard(3);
+function init(){
+    const gameBoard = document.querySelector('.game-board');
+    gameBoard.addEventListener('click', (e)=>{
+    console.log(parseInt(e.target.dataset.cell));
+    });
+    generateGameBoard(4, gameBoard);
+}
+
+init();
